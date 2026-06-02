0a6ff5cdb35b46cbdba8fbe7536dba9c25ea0318
khaled Ibrahiem <khaled.ibrahiem@msa.edu.eg>
2026-06-02 10:02:08 +0300

    v2

---
+++ Functions/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using OpenAI;
+using System.ClientModel;
+using System.Threading.Tasks;
+
+
+namespace Functions
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IConfiguration configuration = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+            var credential = new ApiKeyCredential(configuration["GitHubModels:Token"]);
+
+            var client = new ChatClientBuilder(new OpenAIClient(credential, new OpenAIClientOptions
+            {
+                Endpoint = new Uri("https://models.github.ai/inference")
+
+            }).GetChatClient("openai/gpt-4o-mini").AsIChatClient()).UseFunctionInvocation().Build();
+
+
+            var chatOptions = new ChatOptions
+            {
+                Tools = [AIFunctionFactory.Create((string location, string unit) => {
+
+                var temperature = Random.Shared.Next(5, 20);
+                var conditions = Random.Shared.Next(0, 1) == 0 ? "sunny" : "rainy";
+
+                return $"The weather is {temperature} degrees C and {conditions}.";
+                },
+                "get_current_weather",
+                "Get the current weather in a given location")]
+            };
+
+            List<ChatMessage> chatHistory = [new(ChatRole.System, """
+            You are a hiking enthusiast who helps people discover fun hikes in their area. 
+            You are upbeat and friendly.
+            """)];
+
+            // Weather conversation relevant to the registered function.
+            chatHistory.Add(new(ChatRole.User, """
+            I live in Istanbul and I'm looking for a moderate intensity hike. 
+            What's the current weather like? 
+            """));
+
+            Console.WriteLine($"{chatHistory.Last().Role} >>> {chatHistory.Last()}");
+
+            ChatResponse response = await client.GetResponseAsync(chatHistory, chatOptions);
+
+            chatHistory.Add(new(ChatRole.Assistant, response.Text));
+
+            Console.WriteLine($"{chatHistory.Last().Role} >>> {chatHistory.Last()}");
+
+
+        }
+    }
+}
--- TextCompletions/Program.cs
+++ TextCompletions/Program.cs
@@ -57,14 +57,60 @@ namespace TextCompletions
 
             #region Structured Output
 
-            var prompt = "comapre between any 2 cars and give the output in json format";
-            var response = await client.GetResponseAsync<Car>(prompt);
-            Console.WriteLine(response);
+            //var prompt = "comapre between any 2 cars and give the output in json format";
+            //var response = await client.GetResponseAsync<Car>(prompt);
+            //Console.WriteLine(response);
+
+            #endregion
 
 
+            #region ChtaApp with History
+
+            List<ChatMessage> ChatHistory = new List<ChatMessage>()
+            {
+                new ChatMessage(ChatRole.System, """
+                 You are a friendly hiking enthusiast who helps people discover fun hikes in their area.
+                    You introduce yourself when first saying hello.
+                    When helping people out, you always ask them for this information
+                    to inform the hiking recommendation you provide:
+                
+                    1. The location where they would like to hike
+                    2. What hiking intensity they are looking for
+                
+                    You will then provide three suggestions for nearby hikes that vary in length
+                    after you get that information. You will also share an interesting fact about
+                    the local nature on the hikes when making a recommendation. At the end of your
+                    response, ask if there is anything else you can help with. 
+                """)
+            };
+
+            while (true)
+            {
+                var prompt = Console.ReadLine();
+                Console.WriteLine("User >>> " + prompt);
+                ChatHistory.Add(new ChatMessage(ChatRole.User, prompt));
+
+
+                //var answer = await client.GetResponseAsync(ChatHistory);
+                //Console.WriteLine(answer);
+                //ChatHistory.Add(new ChatMessage(ChatRole.Assistant, answer.Text));
+
+                string res = "";
+                await foreach (var item in client.GetStreamingResponseAsync(ChatHistory))
+                {
+                    Console.Write(item);
+                    res += item.Text;
+                }
+
+                ChatHistory.Add(new ChatMessage(ChatRole.Assistant, res));
+
+
+
+            }
 
 
             #endregion
+
 
 
 
