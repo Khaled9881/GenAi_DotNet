b56004dc37d159f4b469ef9e7ba7cc5d7e4c7ce1
khaled Ibrahiem <khaled.ibrahiem@msa.edu.eg>
2026-06-02 08:16:37 +0300

    v2

--- TextCompletions/Program.cs
+++ TextCompletions/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using OpenAI;
 using System.ClientModel;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 
@@ -27,14 +28,55 @@ namespace TextCompletions
                 .AsIChatClient();
 
 
-            while (true)
-            {
-                var question = Console.ReadLine();
-                var response = await client.GetResponseAsync(question);
-                Console.WriteLine(response);
-            }
+            #region Basic Completion
+            //while (true)
+            //{
+            //    var question = Console.ReadLine();
+            //    var response = await client.GetResponseAsync(question);
+            //    Console.WriteLine(response);
+            //}
+            #endregion
+
+            #region Streaming
+
+            //while (true)
+            //{
+            //    var prompt = Console.ReadLine();
+            //    Console.WriteLine($"user >>>>> {prompt}");
+            //    var answer = client.GetStreamingResponseAsync(prompt);
+            //    await foreach (var item in answer)
+            //    {
+            //        Thread.Sleep(300);
+            //        Console.Write(item);
+            //    }
+
+            //}
 
 
+            #endregion
+
+            #region Structured Output
+
+            var prompt = "comapre between any 2 cars and give the output in json format";
+            var response = await client.GetResponseAsync<Car>(prompt);
+            Console.WriteLine(response);
+
+
+
+
+            #endregion
+
+
+
+        }
+
+        public class Car
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public string Type { get; set; }
+            public int price { get; set; }
+            public string color { get; set; }
         }
     }
 }
